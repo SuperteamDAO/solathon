b92f24c0599607c86a47a06abdaef0ef703288a3
GitBolt <saabisakhtar@gmail.com>
2022-01-17 20:30:48 +0530

    📇Fix request header and paramers encoding

--- solathon/core/http.py
+++ solathon/core/http.py
@@ -1,4 +1,5 @@
 import sys
+import base64
 from typing import Tuple, Any, Dict
 import httpx
 from .. import __version__
@@ -10,11 +11,12 @@ class HTTPClient:
 
     def __init__(self, endpoint: str):
         self.endpoint = endpoint
+        version = sys.version_info
         self.headers = {
             "Content-Type": "application/json",
             "User-Agent": (
-                "Solwarp (https://github.com/GitBolt/solwarp "
-                f"{__version__}) Python{sys.version_info[0]}"
+                "Solathon (https://github.com/GitBolt/solathon "
+                f"{__version__}) Python{version[0]} {version[1]}"
             ),
         }
         self.request_id = 0
@@ -26,9 +28,15 @@ class HTTPClient:
     def build_data(self, method: str, params: Tuple[Any]) -> Dict[str, Any]:
         self.request_id += 1
         params = [str(i) if isinstance(i, PublicKey) else i for i in params]
+
+        if isinstance(params[0], bytes):
+            params[0] = base64.b64encode(params[0]).decode("utf-8")
+
         return {
             "jsonrpc": "2.0",
             "id": self.request_id,
             "method": method,
             "params": params,
         }
+
+
