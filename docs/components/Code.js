d618665135af5cba79a3fec493d14e1b2efee1f8
gitbolt <saabisakhtar@gmail.com>
2022-02-07 21:57:57 +0530

    ☄️ Finish codeblock component

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -1,21 +1,38 @@
-import { useState } from 'react';
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
 import Copy from '../public/icons/Copy.svg'
 import Tick from '../public/icons/Tick.svg'
 
 export const Code = ({ children }) => {
-
     const [icon, setIcon] = useState(Copy)
-    const [code, _] = useState(children.props.children.props.children)
+    const [codeBlock, setCodeBlock] = useState(children)
+    useEffect(() => {
+        let newLinesCodeBlock = {
+            ...children,
+            props: {
+                ...children.props,
+                children: {
+                    ...children,
+                    props: {
+                        ...children.props.children.props,
+                        children: codeBlock.props.children.props.children
+                            .replaceAll(";", "\r")
+                    }
+                }
+            }
+        }
+        setCodeBlock(newLinesCodeBlock)
+    }, [])
 
     const copyToClipboard = () => {
-        navigator.clipboard.writeText(code)
+        navigator.clipboard.writeText(codeBlock.props.children.props.children)
         setIcon(Tick)
         setTimeout(() => setIcon(Copy), 3000)
     }
+
     return (
         <section className="flex items-center relative max-w-fit ">
-            {children}
+            {codeBlock}
             <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
                              ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
                 <Image src={icon} alt="copy" onClick={copyToClipboard} />
