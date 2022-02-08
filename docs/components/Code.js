003346d0e9b813571e637d02b062a49114ea06e6
gitbolt <saabisakhtar@gmail.com>
2022-02-08 23:08:09 +0530

    🐛Debugging prerender error x2

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Copy from '../public/icons/Copy.svg'
 import Tick from '../public/icons/Tick.svg'
 
-export const Code = ({ children } = {props:{children:{props:{children:"Hi"}}}}) => {
+export const Code = ({ children }) => {
   const [icon, setIcon] = useState(Copy)
 
   const copyToClipboard = () => {
@@ -11,24 +11,17 @@ export const Code = ({ children } = {props:{children:{props:{children:"Hi"}}}}) 
     setIcon(Tick)
     setTimeout(() => setIcon(Copy), 3000)
   }
-console.log(children.props.children.props.children.replaceAll(";", "\r"))
+
+  try {
+    children.props.children.props.children.replaceAll()
+  } catch (e) {
+    console.log(e)
+    console.log(children)
+  }
 
   return (
     <section className="flex items-center relative max-w-fit ">
-      {{
-        ...children,
-        props: {
-          ...children.props,
-          children: {
-            ...children,
-            props: {
-              ...children.props.children.props,
-              children: children.props.children.props.children
-                .replaceAll(";", "\r")
-            }
-          }
-        }
-      }}
+      {children}
       <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
                             ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
         <Image src={icon} alt="copy" onClick={copyToClipboard} />
