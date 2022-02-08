9504c1aa18c19a787ef151d59e7423e6f2760977
gitbolt <saabisakhtar@gmail.com>
2022-02-08 19:28:51 +0530

    🔧Fix code new lines for static generation

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -1,40 +1,35 @@
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Copy from '../public/icons/Copy.svg'
 import Tick from '../public/icons/Tick.svg'
 
 export const Code = ({ children }) => {
     const [icon, setIcon] = useState(Copy)
-    const [codeBlock, setCodeBlock] = useState(children)
-    useEffect(() => {
-        let newLinesCodeBlock = {
-            ...children,
-            props: {
-                ...children.props,
-                children: {
-                    ...children,
-                    props: {
-                        ...children.props.children.props,
-                        children: codeBlock.props.children.props.children
-                            .replaceAll(";", "\r")
-                    }
-                }
-            }
-        }
-        setCodeBlock(newLinesCodeBlock)
-    }, [])
 
     const copyToClipboard = () => {
-        navigator.clipboard.writeText(codeBlock.props.children.props.children)
+        navigator.clipboard.writeText(children.props.children.props.children)
         setIcon(Tick)
         setTimeout(() => setIcon(Copy), 3000)
     }
 
     return (
         <section className="flex items-center relative max-w-fit ">
-            {codeBlock}
+            {{
+                ...children,
+                props: {
+                    ...children.props,
+                    children: {
+                        ...children,
+                        props: {
+                            ...children.props.children.props,
+                            children: children.props.children.props.children
+                                .replaceAll(";", "\r")
+                        }
+                    }
+                }
+            }}
             <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
-                             ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
+                            ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
                 <Image src={icon} alt="copy" onClick={copyToClipboard} />
             </div>
         </section>
