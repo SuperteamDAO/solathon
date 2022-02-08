8e0fa9275a98af0b667314106498902f59451908
gitbolt <saabisakhtar@gmail.com>
2022-02-08 22:57:40 +0530

    📇Switched to 2 spaces

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -4,34 +4,34 @@ import Copy from '../public/icons/Copy.svg'
 import Tick from '../public/icons/Tick.svg'
 
 export const Code = ({ children }) => {
-    const [icon, setIcon] = useState(Copy)
+  const [icon, setIcon] = useState(Copy)
 
-    const copyToClipboard = () => {
-        navigator.clipboard.writeText(children.props.children.props.children)
-        setIcon(Tick)
-        setTimeout(() => setIcon(Copy), 3000)
-    }
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(children.props.children.props.children)
+    setIcon(Tick)
+    setTimeout(() => setIcon(Copy), 3000)
+  }
 
-    return (
-        <section className="flex items-center relative max-w-fit ">
-            {{
-                ...children,
-                props: {
-                    ...children.props,
-                    children: {
-                        ...children,
-                        props: {
-                            ...children.props.children.props,
-                            children: children.props.children.props.children
-                                .replaceAll(";", "\r")
-                        }
-                    }
-                }
-            }}
-            <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
+  return (
+    <section className="flex items-center relative max-w-fit ">
+      {{
+        ...children,
+        props: {
+          ...children.props,
+          children: {
+            ...children,
+            props: {
+              ...children.props.children.props,
+              children: children.props.children.props.children
+                .replaceAll(";", "\r")
+            }
+          }
+        }
+      }}
+      <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
                             ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
-                <Image src={icon} alt="copy" onClick={copyToClipboard} />
-            </div>
-        </section>
-    )
+        <Image src={icon} alt="copy" onClick={copyToClipboard} />
+      </div>
+    </section>
+  )
 }
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    content: [
-      "./pages/**/*.js",
-      "./components/**/*.js",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
