2bb20b0858c912af3c89afcb3b3c92e8a31f0c1b
gitbolt <saabisakhtar@gmail.com>
2022-02-08 23:59:25 +0530

    🐛Debugging prerender error x6

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -4,12 +4,12 @@ import { useState } from 'react'
 import Copy from '../public/icons/Copy.svg'
 import Tick from '../public/icons/Tick.svg'
 
-export const Code = ({ children }) => {
+export const Code = ({ children } = null) => {
   const [icon, setIcon] = useState(Copy)
   const router = useRouter()
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(router.isReady ? children.props.children.props.children : {})
+    navigator.clipboard.writeText(router.isReady ? children.props.children.props.children : null)
     setIcon(Tick)
     setTimeout(() => setIcon(Copy), 3000)
   }
@@ -29,7 +29,7 @@ export const Code = ({ children }) => {
             }
           }
         }
-      } : {}}
+      } : null}
       <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
                             ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
         <Image src={icon} alt="copy" onClick={copyToClipboard} />
