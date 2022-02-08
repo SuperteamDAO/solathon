3d4f636bdb8c78e96ec634090672a0d5172b4c74
gitbolt <saabisakhtar@gmail.com>
2022-02-08 23:44:56 +0530

    🐛Debugging prerender error x3

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import Copy from '../public/icons/Copy.svg'
 import Tick from '../public/icons/Tick.svg'
@@ -12,16 +13,22 @@ export const Code = ({ children }) => {
     setTimeout(() => setIcon(Copy), 3000)
   }
 
-  try {
-    children.props.children.props.children.replaceAll()
-  } catch (e) {
-    console.log(e)
-    console.log(children)
-  }
-
   return (
     <section className="flex items-center relative max-w-fit ">
-      {children}
+      {useRouter().isPreview ? {} : {
+        ...children,
+        props: {
+          ...children.props,
+          children: {
+            ...children,
+            props: {
+              ...children.props.children.props,
+              children: children.props.children.props.children
+                .replaceAll(";", "\r")
+            }
+          }
+        }
+      }}
       <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
                             ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
         <Image src={icon} alt="copy" onClick={copyToClipboard} />
