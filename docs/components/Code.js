97b2ea26fc4e9a17d2db0a24bae96c12b17fd4da
gitbolt <saabisakhtar@gmail.com>
2022-02-08 23:56:48 +0530

    🐛Debugging prerender error x5

--- docs/components/Code.js
+++ docs/components/Code.js
@@ -6,19 +6,17 @@ import Tick from '../public/icons/Tick.svg'
 
 export const Code = ({ children }) => {
   const [icon, setIcon] = useState(Copy)
-
-  console.log(useRouter().isPreview)
-  console.log(useRouter().isReady)
+  const router = useRouter()
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(useRouter().isPreview ? {} : children.props.children.props.children)
+    navigator.clipboard.writeText(router.isReady ? children.props.children.props.children : {})
     setIcon(Tick)
     setTimeout(() => setIcon(Copy), 3000)
   }
 
   return (
     <section className="flex items-center relative max-w-fit ">
-      {useRouter().isPreview ? {} : {
+      {useRouter().isReady ? {
         ...children,
         props: {
           ...children.props,
@@ -31,7 +29,7 @@ export const Code = ({ children }) => {
             }
           }
         }
-      }}
+      } : {}}
       <div className={`flex absolute right-3 top-10 w-6 h-6 duration-100
                             ${icon == Copy ? "cursor-pointer active:translate-y-1" : null}`}>
         <Image src={icon} alt="copy" onClick={copyToClipboard} />
