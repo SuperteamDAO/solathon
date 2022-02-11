d036d7e750bb2cca1ff493256626d620458f6f39
gitbolt <saabisakhtar@gmail.com>
2022-02-11 22:57:39 +0530

    🔀Fix float to int

--- solathon/utils.py
+++ solathon/utils.py
@@ -1,6 +1,6 @@
-def lamport_to_sol(lamports: float) -> float:
-    return float(lamports / 1000000000)
+def lamport_to_sol(lamports: int) -> int:
+    return int(lamports / 1000000000)
 
-def sol_to_lamport(lamports: float) -> float:
-    return float(lamports * 1000000000)
+def sol_to_lamport(lamports: int) -> int:
+    return int(lamports * 1000000000)
 
