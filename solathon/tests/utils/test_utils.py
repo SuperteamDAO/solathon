ea333827e6fe37cbe6c57b79089f59dda0ed373d
demetri.bairaktaris <demetri.bairaktaris@imanage.com>
2022-02-13 10:41:59 -0600

    Add some more tests

--- solathon/tests/utils/test_utils.py
+++ solathon/tests/utils/test_utils.py
@@ -2,14 +2,26 @@ import pytest
 from solathon import utils
 
 
+def get_rounded_expectation(arg):
+    return utils.truncate_float(arg * utils.SOL_PER_LAMPORT, utils.SOL_FLOATING_PRECISION)
+
+
 sol_to_lamport_test_data = [
     (0, 0),
     (0.0, 0),
     (1.0, 1000000000),
-    (2.0, 2 * int(utils.LAMPORT_PER_SOL)),
+    (2.0, int(2 * utils.LAMPORT_PER_SOL)),
     (99999.0, int(99999 * utils.LAMPORT_PER_SOL)),
     (123.123, int(123.123 * utils.LAMPORT_PER_SOL)),
     (123123123123.123123123123, int(123123123123.123123123123 * utils.LAMPORT_PER_SOL))
+]
+
+lamport_to_sol_test_data = [
+    (0, get_rounded_expectation(0)),
+    (1, get_rounded_expectation(1)),
+    (2, get_rounded_expectation(2)),
+    (999999999, get_rounded_expectation(999999999)),
+    (9999.999, get_rounded_expectation(9999.999)),
 ]
 
 
@@ -36,3 +48,30 @@ def test_sol_to_lamport_negative_input(arg, expected):
 def test_sol_to_lamport_large_input():
     i = 99999999999999999999999999999.99999999999999999999999999999999
     assert utils.sol_to_lamport(i) == int(i * utils.LAMPORT_PER_SOL)
+
+
+@pytest.mark.parametrize("arg, expected", lamport_to_sol_test_data)
+def test_lamport_to_sol(arg, expected):
+    actual = utils.lamport_to_sol(arg)
+    assert actual == expected
+    assert isinstance(actual, float)
+
+
+def test_lamport_to_sol_none_passed_in():
+    with pytest.raises(TypeError):
+        utils.lamport_to_sol(None)
+
+
+@pytest.mark.parametrize("arg, expected", [(-arg, -expected) for arg, expected in lamport_to_sol_test_data])
+def test_lamport_to_sol_negative_input(arg, expected):
+    actual = utils.lamport_to_sol(arg)
+    assert actual == expected
+    assert isinstance(expected, float)
+    pass
+
+
+def test_lamport_to_sol_large_input():
+    arg = 99999999999999999999999999999999999999999
+    expected = get_rounded_expectation(arg)
+    assert utils.lamport_to_sol(arg) == expected
+    assert isinstance(expected, float)
--- solathon/utils.py
+++ solathon/utils.py
@@ -1,9 +1,18 @@
 LAMPORT_PER_SOL = 1000000000
 SOL_PER_LAMPORT = 1 / LAMPORT_PER_SOL
+SOL_FLOATING_PRECISION = 9
+
+
+def truncate_float(number, length):
+    number = number * pow(10, length)
+    number = int(number)
+    number = float(number)
+    number /= pow(10, length)
+    return number
 
 
 def lamport_to_sol(lamports: int) -> float:
-    return float(lamports / SOL_PER_LAMPORT)
+    return truncate_float(lamports * SOL_PER_LAMPORT, SOL_FLOATING_PRECISION)
 
 
 def sol_to_lamport(sol: float) -> int:
