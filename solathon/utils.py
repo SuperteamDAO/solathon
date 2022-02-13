93c112c5d8da98c079aa41a891d78c5a14a71051
demetri.bairaktaris <demetri.bairaktaris@imanage.com>
2022-02-13 09:05:10 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	solathon/utils.py

--- solathon/utils.py
+++ solathon/utils.py
@@ -2,5 +2,5 @@ def lamport_to_sol(lamports: int) -> float:
     return float(lamports / 1000000000)
 
 
-def sol_to_lamport(sol: float) -> float:
-    return float(sol * 1000000000)
+def sol_to_lamport(sol: float) -> int:
+    return int(sol * 1000000000)
