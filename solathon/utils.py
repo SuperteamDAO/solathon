9339371034321fcb89abb812e7801b851c241b07
demetri.bairaktaris <demetri.bairaktaris@imanage.com>
2022-02-13 08:59:57 -0600

    Fix readability. Fix input for lamports to be int.

--- solathon/utils.py
+++ solathon/utils.py
@@ -1,6 +1,6 @@
-def lamport_to_sol(lamports: float) -> float:
+def lamport_to_sol(lamports: int) -> float:
     return float(lamports / 1000000000)
 
-def sol_to_lamport(lamports: float) -> float:
-    return float(lamports * 1000000000)
 
+def sol_to_lamport(sol: float) -> float:
+    return float(sol * 1000000000)
