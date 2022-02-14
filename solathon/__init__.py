339aead1c305ba301b2f45f1e4a88dd51b9cf59c
gitbolt <saabisakhtar@gmail.com>
2022-02-14 22:07:50 +0530

    ☄️ Add async support

--- solathon/__init__.py
+++ solathon/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.0.4"
 
 from .client import Client
+from .async_client import AsyncClient
 from .publickey import PublicKey
 from .keypair import Keypair
 from .transaction import Transaction
