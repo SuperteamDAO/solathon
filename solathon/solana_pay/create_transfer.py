a0e88982105119e92310d86ab03fd2a7be6562fd
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2023-12-24 16:53:00 +0530

    create transfer init

--- solathon/solana_pay/create_transfer.py
+++ solathon/solana_pay/create_transfer.py
@@ -1,13 +1,38 @@
+from typing import Optional
 from solathon.client import Client
+from solathon.core.instructions import AccountMeta, transfer
+from solathon.core.types import Commitment
 from solathon.publickey import PublicKey
 from solathon.solana_pay.types import CreateTransferFields
+from solathon.transaction import Transaction
+from solathon.utils import sol_to_lamport
 
 
-def create_transfer(client: Client,  sender: PublicKey, transfer_fields: CreateTransferFields):
+def create_transfer(client: Client,  sender: PublicKey, transfer_fields: CreateTransferFields, commitment: Optional[Commitment]=None):
     sender_info = client.get_account_info(sender)
-    
-    recipient_info = client.get_account_info(transfer_fields['recipient'])
+    if transfer_fields.get("recipient", None) == None:
+        raise ValueError("Recipient is missing from transfer_fields")
 
+    if transfer_fields.get("amount", None) == None:
+        raise ValueError("Amount is missing from transfer_fields")
+    lamports = sol_to_lamport(transfer_fields['amount'])
     
+    if lamports > sender_info.result.lamports:
+        raise ValueError("Insufficient funds in sender account")
 
+    instruction = transfer(from_public_key=sender, to_public_key=transfer_fields['recipient'], lamports=lamports)
+
+    if transfer_fields.get("references", None) != None:
+        for ref in transfer_fields['references']:
+            acc_ref = AccountMeta(
+                public_key=ref,
+                is_signer=False,
+                is_writable=False
+            )
+            instruction.keys.append(acc_ref)
+    
+    block_hash = client.get_recent_blockhash(commitment=commitment)
+    transaction = Transaction(instructions=[instruction], signers=[sender], fee_payer=sender,recent_blockhash=block_hash)
+
+    return transaction
 
