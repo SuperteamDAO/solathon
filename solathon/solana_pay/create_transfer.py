77d1e10e1a3a0a46c8cea03efbc8930d31d5ee84
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2023-12-29 01:20:46 +0530

    needs some fixing but we are there

--- solathon/solana_pay/create_transfer.py
+++ solathon/solana_pay/create_transfer.py
@@ -9,9 +9,12 @@ from solathon.utils import sol_to_lamport
 
 
 def create_transfer(client: Client,  sender: PublicKey, transfer_fields: CreateTransferFields, commitment: Optional[Commitment]=None) -> Transaction:
-    sender_info = client.get_account_info(sender)
-    if sender_info['result']['value'] == None:
-        raise ValueError("Sender account does not exist")
+    if client.clean_response == False:
+        raw_sender_info = client.get_account_info(sender)
+        sender_info = raw_sender_info['result']['value']
+    else:
+        sender_info = client.get_account_info(sender)
+    
     
     if transfer_fields.get("recipient", None) == None:
         raise ValueError("Recipient is missing from transfer_fields")
@@ -20,7 +23,7 @@ def create_transfer(client: Client,  sender: PublicKey, transfer_fields: CreateT
         raise ValueError("Amount is missing from transfer_fields")
     lamports = sol_to_lamport(transfer_fields['amount'])
     
-    if lamports > sender_info['result']['value']['lamports']:
+    if lamports > sender_info.lamports:
         raise ValueError("Insufficient funds in sender account")
 
     instruction = transfer(from_public_key=sender, to_public_key=transfer_fields['recipient'], lamports=lamports)
@@ -34,10 +37,13 @@ def create_transfer(client: Client,  sender: PublicKey, transfer_fields: CreateT
             )
             instruction.keys.append(acc_ref)
     
-    block_hash = client.get_recent_blockhash(commitment=commitment)
-    if block_hash['result']['value'] == None:
-        raise ValueError("No recent blockhash found")
-    transaction = Transaction(instructions=[instruction], signers=[sender], fee_payer=sender,recent_blockhash=block_hash['result']['value']['blockhash'])
+    if client.clean_response == False:
+        raw_block_hash = client.get_recent_blockhash(commitment=commitment)
+        block_hash = raw_block_hash['result']['value']
+    else:
+        block_hash = client.get_recent_blockhash(commitment=commitment)
+
+    transaction = Transaction(instructions=[instruction], signers=[sender], fee_payer=sender,recent_blockhash=block_hash.blockhash)
 
     return transaction
 
