2555895f384f980bf8bb253082c3618c983ca49d
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2024-01-08 01:34:38 +0530

    fixed errors and type issues

--- solathon/solana_pay/find_reference.py
+++ solathon/solana_pay/find_reference.py
@@ -20,13 +20,13 @@ def find_reference(client: Client, reference: PublicKey) -> TransactionSignature
     :rtype: solathon.core.types.TransactionSignature
     '''
 
-    signatures: List[TransactionSignature] = None
+    signatures: List[TransactionSignature] = []
     if client.clean_response == False:
-        raw_signatures = client.get_signatures_for_address(reference)
+        raw_signatures = client.get_signatures_for_address(str(reference))
         signatures = [TransactionSignature(signature)
                       for signature in raw_signatures]
     else:
-        signatures = client.get_signatures_for_address(reference)
+        signatures = client.get_signatures_for_address(str(reference))
 
     if len(signatures) == 0:
         raise ValueError("Reference not found")
