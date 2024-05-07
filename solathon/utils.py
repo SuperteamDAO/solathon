e12608d4b6d637768f9c1b214c25016e22351e9d
Suhas Sumukh <suhasasumukh@gmail.com>
2024-05-07 19:43:06 +0530

    floating point precision

--- solathon/utils.py
+++ solathon/utils.py
@@ -1,31 +1,25 @@
 from __future__ import annotations
 
-from typing import Any
-from .publickey import PublicKey
+from typing import Any, Union
 from nacl.signing import VerifyKey
+from .publickey import PublicKey
 from solathon.core.types import Commitment, RPCErrorType, RPCResponse
 
 class RPCRequestError(Exception):
-    def __init__(self, message="Failed to fetch data from RPC endpoint"):
+    def __init__(self, message: str = "Failed to fetch data from RPC endpoint"):
         self.message = message
         super().__init__(self.message)
 
 
-LAMPORT_PER_SOL: int = 1000000000
-SOL_PER_LAMPORT: float = 1 / LAMPORT_PER_SOL
+LAMPORTS_PER_SOL: int = 1_000_000_000
 SOL_FLOATING_PRECISION: int = 9
 
 
 def truncate_float(number: float, length: int) -> float:
-    number = number * pow(10, length)
-    number = int(number)
-    number = float(number)
-    number /= pow(10, length)
-    return number
+    return float(f'{number:.{length}f}')
 
 
 def validate_commitment(value: Commitment) -> Commitment:
-    # If the types change make the same change in the type hint
     allowed_commitments = {"processed", "confirmed", "finalized",
                            "recent", "single", "singleGossip", "root", "max"}
 
@@ -35,35 +29,31 @@ def validate_commitment(value: Commitment) -> Commitment:
 
     return value
 
-def lamport_to_sol(lamports: int) -> float:
-    return truncate_float(lamports * SOL_PER_LAMPORT, SOL_FLOATING_PRECISION)
+
+def lamports_to_sol(lamports: int) -> float:
+    return truncate_float(lamports / LAMPORTS_PER_SOL, SOL_FLOATING_PRECISION)
 
 
-def sol_to_lamport(sol: float | int) -> int:
-    return int(sol * LAMPORT_PER_SOL)
+def sol_to_lamports(sol: Union[float, int]) -> int:
+    return int(sol * LAMPORTS_PER_SOL)
 
 
 def verify_signature(
-    public_key: PublicKey | str,
-    signature: list,
-    message: bytes | str | None = None
+    public_key: Union[PublicKey, str],
+    signature: bytes,
+    message: Union[bytes, None] = None
 ) -> None:
-
     if not message:
-        message = public_key.base58_encode()
+        message = bytes(public_key)
 
     if isinstance(public_key, str):
         public_key = PublicKey(public_key)
 
-    if isinstance(message, str):
-        message = bytes(message, encoding="utf8")
-
-    bytes_pk = bytes(public_key)
-    vk = VerifyKey(bytes_pk)
-    vk.verify(message, bytes(signature))
+    vk = VerifyKey(bytes(public_key))
+    vk.verify(message, signature)
 
 
-def clean_response(response: RPCResponse) -> dict[str, Any] | RPCErrorType:
+def clean_response(response: RPCResponse) -> Union[dict[str, Any], RPCErrorType]:
     if "error" in response:
         return response["error"]
 
